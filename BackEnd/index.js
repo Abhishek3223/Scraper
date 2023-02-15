7a5b36968e6797968eefbad630b8b9e8bc587a4f
abhishek ranjan <imabhishekranjann1001@gmail.com>
2023-02-15 18:42:30 +0530

    routing issues for deployment

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -23,9 +23,9 @@ app.get('/', function (req, res) {
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 app.use('/api/comment', require('./routes/Comment'))
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
