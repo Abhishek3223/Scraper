571076f7b1f8fc69f01c59dcb76b664d8f3e638b
abhishek ranjan <imabhishekranjann1001@gmail.com>
2023-02-15 22:21:38 +0530

    added start

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 const { send } = require('process');
 const app = Express()
 app.use(cors())
-const port = 5000
+const port = process.env.PORT || 5000
 
 
 app.use(Express.json())
@@ -23,9 +23,7 @@ app.get('/', function (req, res) {
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 app.use('/api/comment', require('./routes/Comment'))
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
