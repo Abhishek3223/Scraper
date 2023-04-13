dac613652e5743db58b06e9b8b910a9db15b0248
abhishek ranjan <imabhishekranjann1001@gmail.com>
2023-04-14 01:12:30 +0530

    made few changes

--- Scraper/automation.js
+++ Scraper/automation.js
@@ -4,6 +4,7 @@ const run = require('./linkLoader')
 cron.schedule('0 */2 * * * *', () => {
   console.log('running a task every minute');
   const date = new Date();
+  console.log(date);
   run();
   console.log(date);
 });
