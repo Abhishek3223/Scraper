3cc448214000d69bf3932c3e777eb5b2fe7f99cc
abhishek ranjan <imabhishekranjann1001@gmail.com>
2023-06-04 06:16:55 +0530

    updated the time for re-rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,18 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 const spinner = document.getElementById('spinner');
 
 // Hide the spinner initially
 spinner.style.display = 'none';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// Show the spinner while React is loading
-spinner.style.display = 'block';
+// Show the spinner while waiting
+spinner.style.display = 'flex';
 
-// Hide the spinner and render data after a 3-second buffer time
-setTimeout(() => {
-  spinner.style.display = 'none';
-  // Render the data or perform any other action here
-}, 3000);
-
-// Example of rendering the data after the buffer time
+// Wait for 3 seconds before re-rendering the App component
 setTimeout(() => {
   ReactDOM.render(
     <React.StrictMode>
@@ -39,16 +20,12 @@ setTimeout(() => {
     </React.StrictMode>,
     document.getElementById('root')
   );
+
+  // Hide the spinner after rendering the App component
+  spinner.style.display = 'none';
 }, 3000);
 
-// You can also call the reportWebVitals function after the buffer time
+// Call the reportWebVitals function after the waiting time
 setTimeout(() => {
   reportWebVitals();
 }, 3000);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// You can also call the reportWebVitals function after the buffer time
-// setTimeout(() => {
-//   reportWebVitals();
-// }, 3000);
